5c537347d350cdc405f9a57965fa3f8af4b7c658
Victor <vintorli@gmail.com>
2018-05-09 17:39:37 +0200

    Trying to save train and output pickles.

--- transfer_learning.py
+++ transfer_learning.py
@@ -1,13 +1,11 @@
 #!/srv/glusterfs/vli/.pyenv/shims/python
 
 import torch, torchvision, os, datetime, time, math, pandas as pd, sys, random, statistics, numpy as np, scipy, pickle
-from sklearn.neighbors.kde import KernelDensity
-
 
 sys.path.append('/home/vli/webcam-location') # For importing .py files in the same directory on the cluster.
 import constants
 from webcam_dataset import WebcamData
-from webcam_dataset import Train
+from webcam_dataset import Train, Test
 from custom_transforms import RandomResize, Resize, RandomPatch, Center, ToTensor
 from custom_model import WebcamLocation
 from torch.autograd import Variable
@@ -50,11 +48,14 @@ sunset_model.eval()
 
 data = WebcamData()
 days = data.days
-if constants.CENTER: # Should I use RandomResize or Resize? # VLI
+if constants.CENTER: # Should I use RandomResize or Resize for Train? # VLI
     train_transformations = torchvision.transforms.Compose([Resize(), RandomPatch(constants.PATCH_SIZE), Center(), ToTensor()])
+    test_transformations = torchvision.transforms.Compose([Resize(), RandomPatch(constants.PATCH_SIZE), Center(), ToTensor()])
 else:
     train_transformations = torchvision.transforms.Compose([Resize(), RandomPatch(constants.PATCH_SIZE), ToTensor()])
+    test_transformations = torchvision.transforms.Compose([Resize(), RandomPatch(constants.PATCH_SIZE), ToTensor()])
 train_dataset = Train(data, train_transformations)
+test_dataset = Test(data, test_transformations)
 
 if torch.cuda.is_available():
     pin_memory = True
@@ -65,9 +66,14 @@ else:
     num_workers = constants.NUM_LOADER_WORKERS
 
 train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=constants.BATCH_SIZE, num_workers=num_workers, pin_memory=pin_memory)
+test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=constants.BATCH_SIZE, num_workers=num_workers, pin_memory=pin_memory)
 
-sunrise_input = np.zeros((len(train_loader.dataset), sunrise_model.first_fc_layer_size))
-sunrise_output = np.zeros((len(train_loader.dataset), 1))
+# Training Set
+
+# sunrise
+
+train_sunrise_input = np.zeros((len(train_loader.dataset), sunrise_model.first_fc_layer_size))
+train_sunrise_output = np.zeros((len(train_loader.dataset), 1))
 sunrise_predict_t0 = time.time()
 for batch_idx, (input, target) in enumerate(train_loader):
     input = Variable(input, volatile=True)
@@ -75,14 +81,20 @@ for batch_idx, (input, target) in enumerate(train_loader):
     if torch.cuda.is_available():
         input = input.cuda()
 
-    sunrise_features = sunrise_model.forward_features(input) # Batches...
-
+    sunrise_features = sunrise_model.forward_features(input)
+    end = min(len(train_loader.dataset), (batch_idx + 1) * constants.BATCH_SIZE)
+    train_sunrise_input[batch_idx * constants.BATCH_SIZE:end, :] = sunrise_features.numpy()
+    train_sunrise_output[batch_idx * constants.BATCH_SIZE:end, :] = target.numpy()
 
 
 sunrise_predict_t1 = time.time()
-print('Sunrise prediction time (min): {:.2f}'.format((sunrise_predict_t1 - sunrise_predict_t0) / 60))
+print('Sunrise training prediction time (min): {:.2f}'.format((sunrise_predict_t1 - sunrise_predict_t0) / 60))
 sys.stdout.flush()
 
+# sunset
+
+train_sunset_input = np.zeros((len(train_loader.dataset), sunset_model.first_fc_layer_size))
+train_sunset_output = np.zeros((len(train_loader.dataset), 1))
 sunset_predict_t0 = time.time()
 for batch_idx, (input, target) in enumerate(train_loader):
     input = Variable(input, volatile=True)
@@ -91,9 +103,37 @@ for batch_idx, (input, target) in enumerate(train_loader):
         input = input.cuda()
 
     sunset_features = sunset_model.forward_features(input)
-
+    end = min(len(train_loader.dataset), (batch_idx + 1) * constants.BATCH_SIZE)
+    train_sunset_input[batch_idx * constants.BATCH_SIZE:end, :] = sunset_features.numpy()
+    train_sunset_output[batch_idx * constants.BATCH_SIZE:end, :] = target.numpy()
 
 sunset_predict_t1 = time.time()
-print('Sunset prediction time (min): {:.2f}'.format((sunset_predict_t1 - sunset_predict_t0) / 60))
+print('Sunset training prediction time (min): {:.2f}'.format((sunset_predict_t1 - sunset_predict_t0) / 60))
 sys.stdout.flush()
 
+# Pickle training features, output
+
+if constants.CLUSTER:
+    dir = '/srv/glusterfs/vli/features/'
+else:
+    dir = '/home/vli/features/'
+
+with open(dir + str(constants.DAYS_PER_MONTH) + '_sunrise_train_input.pkl', 'wb') as f:
+    pickle.dump(train_sunrise_input, f)
+with open(dir + str(constants.DAYS_PER_MONTH) + '_sunrise_train_output.pkl', 'wb') as f:
+    pickle.dump(train_sunrise_output, f)
+with open(dir + str(constants.DAYS_PER_MONTH) + '_sunset_train_input.pkl', 'wb') as f:
+    pickle.dump(train_sunset_input, f)
+with open(dir + str(constants.DAYS_PER_MONTH) + '_sunset_train_output.pkl', 'wb') as f:
+    pickle.dump(train_sunset_output, f)
+
+del train_sunrise_input
+del train_sunrise_output
+del train_sunset_input
+del train_sunset_output
+
+# Test Set
+
+# sunrise
+
+# TO DO # VLI
