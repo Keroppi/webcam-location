e2dc9f4dc1da78f05a44aed71b1186f622a21f9d
Victor <vintorli@gmail.com>
2018-05-14 17:31:34 +0200

    max days

--- constants.py
+++ constants.py
@@ -1,12 +1,12 @@
 CLUSTER = True
-LEARNING_SUNRISE = False # True for sunrise, False for sunset
-DAYS_PER_MONTH = 5 # 'MAX'
+LEARNING_SUNRISE = True # True for sunrise, False for sunset
+DAYS_PER_MONTH = 'MAX' # 'MAX'
 SIZE = ['small'] # 'large'
 #DATA_SOURCES = ['roundshot'] # 'panomax'
 IMAGES_PER_DAY = 32
 PATCH_SIZE = (128, 128) # height, width
 NUM_CHANNELS = 3 # RGB or YCbCr
-BATCH_SIZE = 120
+BATCH_SIZE = 130
 NUM_LOADER_WORKERS = 2
 EPOCHS = 200
 LOG_INTERVAL = 20
