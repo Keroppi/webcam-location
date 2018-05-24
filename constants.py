641e6761eb6c5ea6faa1863b79402c8b789edb46
Victor <vintorli@gmail.com>
2018-05-24 21:52:48 +0200

    Sunrise

--- constants.py
+++ constants.py
@@ -1,12 +1,12 @@
 CLUSTER = True
-LEARNING_SUNRISE = False # True for sunrise, False for sunset
+LEARNING_SUNRISE = True # True for sunrise, False for sunset
 DAYS_PER_MONTH = 'MAX'
 SIZE = ['small'] # 'large'
 #DATA_SOURCES = ['roundshot'] # 'panomax'
 IMAGES_PER_DAY = 32
 PATCH_SIZE = (128, 128) # height, width
 NUM_CHANNELS = 3 # RGB or YCbCr
-BATCH_SIZE = 260
+BATCH_SIZE = 160
 NUM_LOADER_WORKERS = 4 # VLI
 EPOCHS = 200
 LOG_INTERVAL = 20
