07f6d5e5b3b4d9809c4341237321e5188515ffa3
Victor <vintorli@gmail.com>
2018-06-14 19:50:39 +0200

    .

--- constants.py
+++ constants.py
@@ -1,19 +1,19 @@
 CLUSTER = True
 LEARNING_SUNRISE = True # True for sunrise, False for sunset
-DAYS_PER_MONTH = 3 #'MAX'
+DAYS_PER_MONTH = 'MAX'
 SIZE = ['small'] # 'large'
 #DATA_SOURCES = ['roundshot'] # 'panomax'
 IMAGES_PER_DAY = 32
 PATCH_SIZE = (128, 128) # height, width
 NUM_CHANNELS = 3 # RGB or YCbCr
 BATCH_SIZE = 160
-NUM_LOADER_WORKERS = 4
+NUM_LOADER_WORKERS = 3
 EPOCHS = 200
 LOG_INTERVAL = 20
 SPLIT_TOTAL = 100
 SPLIT_TRAIN = 75 # 75% train
 SPLIT_TEST = 100 # 25% test, 0% validation
-BANDWIDTH = 0.25
+BANDWIDTH = 0.125
 CENTER = False
 LAMBDA = 1 # REGULARIZER
 
