7d68f8b514e7a6e948d65adff928f7479d606b71
Victor Li <vli@student.ethz.ch>
2018-08-07 02:13:54 +0200

    .

--- constants.py
+++ constants.py
@@ -13,7 +13,7 @@ LOG_INTERVAL = 20
 SPLIT_TOTAL = 100
 SPLIT_TRAIN = 75 # 75% train
 SPLIT_TEST = 100 # 25% test, 0% validation
-BANDWIDTH = 0.05
+BANDWIDTH = 0.3
             # 0.3 -
             # 0.2 - 320
             # 0.175 -
@@ -21,7 +21,7 @@ BANDWIDTH = 0.05
             # 0.125 - 324
             # 0.1 -
             # 0.075 -
-            # 0.05 -
+            # 0.05 - 345
 
             ## 0.3 -
             ## 0.25 -
@@ -31,12 +31,12 @@ BANDWIDTH = 0.05
             ## 0.05 -
 CENTER = False
 LAMBDA = 1 # REGULARIZER
-INLIER_THRESHOLD = 400 # km
+INLIER_THRESHOLD = 200 # km
                    # 800 - 301
                    # 600 - 320
                    # 500 -
                    # 450 -
-                   # 400 -
+                   # 400 - 350
                    # 350 -
                    # 300 -
                    # 200 -
@@ -46,14 +46,13 @@ INLIER_THRESHOLD = 400 # km
                    ## 600 -
                    ## 700 -
                    ## 800 -
-AZIMUTHAL_INLIER_THRESHOLD = 0.06283185307179587 # 400 km -
-                             # 0.1411098700237413 # 900 km -
+AZIMUTHAL_INLIER_THRESHOLD = 0.1411098700237413 # 900 km -
 # 0.12566370614359174 # 800 km - 297
                              # 0.10978120995044331 # 700 km -
                              # 0.09407324668249435 # 600 km - 319
                              # 0.07853981633974483 # 500 km -
                              # 0.07068583470577036 # 450 km -
-
+# 0.06283185307179587 # 400 km - 354
                              # 0.05497787143782138 # 350 km -
                              # 0.0470366233412471 # 300 km -
 
@@ -63,21 +62,22 @@ AZIMUTHAL_INLIER_THRESHOLD = 0.06283185307179587 # 400 km -
                              ## 600 -
                              ## 700 -
                              ## 800 -
-MAHALANOBIS_INLIER_THRESHOLD = 0.5
+MAHALANOBIS_INLIER_THRESHOLD = 1.5
                                # 2 - 302
                                # 1.75 -
                                # 1.5 -
                                # 1.25 - 304
                                # 1 -
-                               # 0.5 -
+                               # 0.5 - 341
 
                                ## 2 -
                                ## 1.5 -
                                ## 1 -
                                ## 0.5 -
-AZIMUTHAL_MAHALANOBIS_INLIER_THRESHOLD = 0.5
+AZIMUTHAL_MAHALANOBIS_INLIER_THRESHOLD = 0.75
                                          # 2 - 422
                                          # 1 - 283
+                                         # 0.5 - 312
 BIGM = 100
 
 # 1 pass
