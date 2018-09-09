5913fed227ce2176096c03b8432ab91748f0556d
Victor Li <vli@student.ethz.ch>
2018-09-10 01:25:10 +0200

    .

--- use_pred_pkl.py
+++ use_pred_pkl.py
@@ -78,8 +78,8 @@ for place in predictions:
         solstice_days = days_from_solstice(predictions[place][d_idx].sunrise - datetime.timedelta(seconds=predictions[place][d_idx].time_offset))
 
         # VLI
-        if equinox_days < constants.EQUINOX_DISCARD_DAYS: # ? weeks
-            continue
+        #if equinox_days < constants.EQUINOX_DISCARD_DAYS: # ? weeks
+        #    continue
 
         days += [day]
 
@@ -1045,10 +1045,10 @@ def plot_map(lats, lngs, mean_locations, median_locations, density_locations, ra
 
         plt.title(place)
 
-        if not os.path.isdir('/srv/glusterfs/vli/maps2/' + mode + '/'):
-            os.mkdir('/srv/glusterfs/vli/maps2/' + mode + '/')
+        if not os.path.isdir('/srv/glusterfs/vli/maps3/' + mode + '/'):
+            os.mkdir('/srv/glusterfs/vli/maps3/' + mode + '/')
 
-        plt.savefig('/srv/glusterfs/vli/maps2/' + mode + '/' + place + '.png')
+        plt.savefig('/srv/glusterfs/vli/maps3/' + mode + '/' + place + '.png')
         plt.close()
 
     map_t1 = time.time()
@@ -1297,7 +1297,7 @@ def scatter(days_used, distances, fmt, label, color=None, linestyle=None, marker
     else:
         prefix = ''
 
-    plt.savefig('/srv/glusterfs/vli/maps2/' + prefix + label + '_days_used.png')
+    plt.savefig('/srv/glusterfs/vli/maps3/' + prefix + label + '_days_used.png')
     plt.close()
 
 scatter_t0 = time.time()
@@ -1344,7 +1344,7 @@ def bar(x, y, ylabel, xlabel, x_labels, title, filename, yerr=None, ymax=None):
         ax.set_ylim([0, ymax])
 
     plt.title(title)
-    plt.savefig('/srv/glusterfs/vli/maps2/' + filename)
+    plt.savefig('/srv/glusterfs/vli/maps3/' + filename)
     plt.close()
 
 # Plot average distance error vs. time interval OVER ALL DAYS.
