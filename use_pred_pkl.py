9ff08b18b26962e0d8c337de04050af77dd1fb66
Victor Li <vli@student.ethz.ch>
2018-09-12 13:03:25 +0200

    .

--- use_pred_pkl.py
+++ use_pred_pkl.py
@@ -1002,10 +1002,10 @@ def plot_map(lats, lngs, mean_locations, median_locations, density_locations, ra
 
         plt.title(place)
 
-        if not os.path.isdir('/srv/glusterfs/vli/maps2/' + mode + '/'):
-            os.mkdir('/srv/glusterfs/vli/maps2/' + mode + '/')
+        if not os.path.isdir('/srv/glusterfs/vli/maps3/' + mode + '/'):
+            os.mkdir('/srv/glusterfs/vli/maps3/' + mode + '/')
 
-        plt.savefig('/srv/glusterfs/vli/maps2/' + mode + '/' + place + '.png', dpi=100)
+        plt.savefig('/srv/glusterfs/vli/maps3/' + mode + '/' + place + '.png', dpi=100)
         plt.close()
 
     map_t1 = time.time()
@@ -1256,7 +1256,7 @@ def scatter(days_used, distances, fmt, label, color=None, linestyle=None, marker
     else:
         prefix = ''
 
-    plt.savefig('/srv/glusterfs/vli/maps2/' + prefix + label + '_days_used.png', dpi=100)
+    plt.savefig('/srv/glusterfs/vli/maps3/' + prefix + label + '_days_used.png', dpi=100)
     plt.close()
 
 # VLI
@@ -1306,7 +1306,7 @@ def bar(x, y, ylabel, xlabel, x_labels, title, filename, yerr=None, ymax=None):
         ax.set_ylim([0, ymax])
 
     plt.title(title)
-    plt.savefig('/srv/glusterfs/vli/maps2/' + filename, dpi=100)
+    plt.savefig('/srv/glusterfs/vli/maps3/' + filename, dpi=100)
     plt.close()
 
 # Plot average distance error vs. time interval OVER ALL DAYS.
