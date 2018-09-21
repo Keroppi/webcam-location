1df3540f19dfd1e17ee666c1cb39bdd92cf80b2f
Victor Li <vli@student.ethz.ch>
2018-09-21 13:31:24 +0200

    .

--- use_pred_pkl.py
+++ use_pred_pkl.py
@@ -1018,10 +1018,10 @@ def plot_map(lats, lngs, mean_locations, median_locations, density_locations, ra
 
         plt.title(place)
 
-        if not os.path.isdir('/srv/glusterfs/vli/maps1/' + mode + '/'):
-            os.mkdir('/srv/glusterfs/vli/maps1/' + mode + '/')
+        if not os.path.isdir('/srv/glusterfs/vli/maps2/' + mode + '/'):
+            os.mkdir('/srv/glusterfs/vli/maps2/' + mode + '/')
 
-        plt.savefig('/srv/glusterfs/vli/maps1/' + mode + '/' + place + '.png', dpi=100)
+        plt.savefig('/srv/glusterfs/vli/maps2/' + mode + '/' + place + '.png', dpi=100)
         plt.close()
 
     map_t1 = time.time()
@@ -1272,7 +1272,7 @@ def scatter(days_used, distances, fmt, label, color=None, linestyle=None, marker
     else:
         prefix = ''
 
-    plt.savefig('/srv/glusterfs/vli/maps1/' + prefix + label + '_days_used.png', dpi=100)
+    plt.savefig('/srv/glusterfs/vli/maps2/' + prefix + label + '_days_used.png', dpi=100)
     plt.close()
 
 # VLI
@@ -1322,7 +1322,7 @@ def bar(x, y, ylabel, xlabel, x_labels, title, filename, yerr=None, ymax=None):
         ax.set_ylim([0, ymax])
 
     plt.title(title)
-    plt.savefig('/srv/glusterfs/vli/maps1/' + filename, dpi=100)
+    plt.savefig('/srv/glusterfs/vli/maps2/' + filename, dpi=100)
     plt.close()
 
 # Plot average distance error vs. time interval OVER ALL DAYS.
@@ -1602,7 +1602,7 @@ plt.legend(handlelist, legend_labels)
 plt.ylabel('# Days')
 plt.xlabel('Error (min)')
 plt.title('# of Days vs. Sunrise and Sunset Error (min)')
-plt.savefig('/srv/glusterfs/vli/maps1/sunrise_sunset_err.png', dpi=100)
+plt.savefig('/srv/glusterfs/vli/maps2/sunrise_sunset_err.png', dpi=100)
 plt.close()
 
 # Plot # of days vs. sunrise/sunset OUTLIER errs (min) over ALL DAYS.
@@ -1647,7 +1647,7 @@ plt.legend(handlelist, legend_labels)
 plt.ylabel('# Days')
 plt.xlabel('Error (min)')
 plt.title('# of Days vs. Sunrise and Sunset Error (min)')
-plt.savefig('/srv/glusterfs/vli/maps1/sunrise_sunset_err_outliers.png', dpi=100)
+plt.savefig('/srv/glusterfs/vli/maps2/sunrise_sunset_err_outliers.png', dpi=100)
 plt.close()
 
 print('# outliers for sunrise err: {}'.format(sum(sunrise_buckets)))
@@ -1702,7 +1702,7 @@ plt.gca().set_ylim([0, 4500])
 plt.ylabel('# Days')
 plt.xlabel('Signed Sunrise Error (min)')
 plt.title('# of Days vs. Signed Sunrise Error (min)')
-plt.savefig('/srv/glusterfs/vli/maps1/sunrise_signed_err.png', dpi=100)
+plt.savefig('/srv/glusterfs/vli/maps2/sunrise_signed_err.png', dpi=100)
 plt.close()
 
 plt.figure(figsize=(14.4,7.2))
@@ -1718,7 +1718,7 @@ plt.gca().set_ylim([0, 4500])
 plt.ylabel('# Days')
 plt.xlabel('Signed Sunset Error (min)')
 plt.title('# of Days vs. Signed Sunset Error (min)')
-plt.savefig('/srv/glusterfs/vli/maps1/sunset_signed_err.png', dpi=100)
+plt.savefig('/srv/glusterfs/vli/maps2/sunset_signed_err.png', dpi=100)
 plt.close()
 
 
